99e37caa865973bb88a40d5a2ec6facd204b27b4
Glitch (peaceful-wakeful-iguanodon) <none>
2020-06-04 17:59:11 +0000

    bai3-Query parameters

--- server.js
+++ server.js
@@ -5,13 +5,23 @@
 // but feel free to use whatever libraries or frameworks you'd like through `package.json`.
 const express = require("express");
 const app = express();
+var todos = ['Đi Chợ','Nấu Cơm','Rửa Bát','Học Tại CoderX'];
 app.set("view engine", "pug");
 app.set("views", "./views");
+
 // https://expressjs.com/en/starter/basic-routing.html
 app.get("/", (req, res) => {
-  res.render("todos", { todos: ["Đi Chợ", "Nấu Cơm", "Rửa Bát" , "Học Tại CoderX"] });
+  res.render('todos',{todos:todos});
 });
-
+app.get('/todos/search',( req , res)=>{
+	var q = req.query.q;
+	var matchedUsers = todos.filter((todo)=>{
+		return (todo.toLowerCase().indexOf(q.toLowerCase()) !== -1);
+	});
+	res.render('todos',{
+		todos: matchedUsers
+	});
+});
 // listen for requests :)
 app.listen(process.env.PORT, () => {
   console.log("Server listening on port " + process.env.PORT);
